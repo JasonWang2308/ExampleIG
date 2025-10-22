353f18184648a1ae23df69fa7e18cb74f14032f4
Jason Wang <JasonWang2308@users.noreply.github.com>
2025-10-22 11:39:48 +0800

    delete app+

--- app/service-worker.js
+++
@@ -1,5 +0,0 @@
-// app/service-worker.js（超精簡，不做快取，只為了讓 PWA 可安裝）
-self.addEventListener("install", () => { /* noop */ });
-self.addEventListener("activate", () => { /* noop */ });
-// 有一個 fetch 事件處理器，讓 Chrome 覺得這是可控頁面
-self.addEventListener("fetch", (event) => { event.respondWith(fetch(event.request)); });
